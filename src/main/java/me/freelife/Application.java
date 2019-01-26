4f814cbfe6a906e7419af475094597f7f718aaac
freelife1191 <freelife1191.good@gmail.com>
2019-01-27 01:01:28 +0900

    @EnableAsync 제거

--- src/main/java/me/freelife/Application.java
+++ src/main/java/me/freelife/Application.java
@@ -2,10 +2,8 @@ package me.freelife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class Application {
 
     public static void main(String[] args) {
